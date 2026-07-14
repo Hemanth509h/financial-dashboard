7b2dbf0c3f22be09c7ab5e4cfd7684cc30bf0a79
Hemanth <phemanthkumar509@gmail.com>
2026-07-14 21:08:11 +0530

    Update API_URL to use a hardcoded value instead of environment variable

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, '') || '';
+const API_URL = 'http://10.0.2.15:3000';
 
 const client = axios.create({
   baseURL: `${API_URL}/api`,
